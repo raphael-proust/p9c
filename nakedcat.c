eefe602466e686ac63e74d98d9a39d1a2e5937ee
Raphaël Proust <raphlalou@gmail.com>
2012-10-14 15:30:15 +0100

    [nakedcat] simpler read vs write checks

--- nakedcat.c
+++ nakedcat.c
@@ -5,15 +5,14 @@ void
 main(int argc, char *argv[])
 {
 	int i, source;
-	long read_rc, write_rc;
+	long bytecount;
 	char * buffer[256];
 	for(i=1; i<argc; i++){
 		source = open (argv[i], OREAD);
 		do {
-			read_rc = read(source, buffer, 256);
-			write_rc = write(1, buffer, read_rc);
-			if (write_rc !=read_rc)
+			bytecount = read(source, buffer, 256);
+			if(write(1, buffer, bytecount) != bytecount)
 				exits("Writing error");
-		} while (write_rc > 0);
+		} while (bytecount > 0);
 	}
 }
